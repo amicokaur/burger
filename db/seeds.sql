82ffe53001d85c385e3f168ab2690cde56b6d33e
amicokaur <amica0811@yaho..com>
2018-09-13 21:09:49 -0700

    1push

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger with Peanut');
+INSERT INTO burgers (burger_name) VALUES ('Amica-Burger);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Umami Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Green-Chile Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger on Brioche Buns', true);
+INSERT INTO burgers (burger_name) VALUES ('Pug Burger');
